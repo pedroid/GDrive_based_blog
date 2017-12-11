9fd0513728958061642185889722d4f0e9faa51f
yushengc <ntuyusheng@gmail.com>
2017-12-11 20:47:04 +0800

    20171212

--- js/index.js
+++ js/index.js
@@ -7,14 +7,15 @@ $(function(){
 		"command":"commandGetBlogs"
 		
 	},function(data){
-		//console.log(data);
+		console.log(data);
 		var blogs_set = data.split('||');
 		blogs_set.pop();
 		//console.log(blogs_set);
 		test = blogs_set;
 		for(i in blogs_set){
 			//$('.content-wrapper').prepend("<div>test</div>")
-			$('#LatestArticle').prepend("<div class=\"grid-item "+blogs_set[i].split("$$")[3]+"\ grid-item-blue\"><div class=\"description\"><div class=\"item-title\"><a target=\"_blank\"href=\"blog.html?FileID="+blogs_set[i].split("$$")[0]+"&FolderID="+blogs_set[i].split("$$")[1]+"\">"+blogs_set[i].split("$$")[2]+"</a></div><div class=\"item-tags\"><div class=\"tag\">"+blogs_set[i].split("$$")[3]+"</div></div></div></div>")
+			//console.log(blogs_set[i].split("$$")[4]);
+			$('#LatestArticle').prepend("<div class=\"grid-item "+blogs_set[i].split("$$")[3]+"\ grid-item-blue\"><div class=\"description\"><div class=\"item-title\"><a target=\"_blank\"href=\"blog.html?FileID="+blogs_set[i].split("$$")[0]+"&FolderID="+blogs_set[i].split("$$")[1]+"\">"+blogs_set[i].split("$$")[2]+"</a></div><div class=\"item-tags\"><div class=\"tag\">"+blogs_set[i].split("$$")[3]+"</div><div class=\"thumb-title\"><img src=\""+blogs_set[i].split("$$")[4]+"\"/></div></div></div></div>")
 			//$('.content-wrapper').prepend("<div class=\"grid-item teaching_plan\"><div class=\"description\"><div class=\"item-title\"><a href="">"+blogs_set[i].split('$$')[0]+"</div><div class=\"item-tags\"><div class=\"tag\">"+blogs_set[i].split('$$')[1]+"</div></div></div></div>")
 			
 		}
