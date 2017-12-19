b72670dda300d960b7715a96d7884a5b7d8047e6
yushengc <ntuyusheng@gmail.com>
2017-12-19 22:34:42 +0800

    20171219

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,12 @@
 var appFiles = "https://script.google.com/macros/s/AKfycby8FMMIJQkm0lAqUzyP_epJiw1dP3JMZ8VdiTGHckpfEVpecs-N/exec";
-
+var appBlogAuthen = "https://script.google.com/macros/s/AKfycby1kO-bI-2KeFM2XKHVRZwsS9cUjoJyklfOsGwP-h3S4c_sZUg/exec";
 $(function(){
+		load();
+
+
+});
+
+var load = function(){
 	//console.log('test');
 	prettyPrint();
 	$.get(appFiles, {
@@ -25,9 +31,9 @@ $(function(){
 			var text_draft = (files_draft=="1")?"(draft)":"";
 			var text_public = (files_public=="1")?"":"(private)";
 			var grid_item_text;
-			if( (files_draft=="0")&(files_public=="1") ) {grid_item_text = "grid-item-blue";}
+			if( (files_draft=="0")&(files_public=="1") ) {grid_item_text = "grid-item-dodger";}
 			if( (files_draft=="0")&(files_public=="0") ) {grid_item_text = "grid-item-red";}
-			if( (files_draft=="1")&(files_public=="1") ) {grid_item_text = "grid-item-orange";}
+			if( (files_draft=="1")&(files_public=="1") ) {grid_item_text = "grid-item-sky";}
 			if( (files_draft=="1")&(files_public=="0") ) {grid_item_text = "grid-item-safe";}
 			$('#LatestArticle').prepend("<div class=\"grid-item "+folders_name+"\ "+grid_item_text+"\"><div class=\"description\"><div class=\"item-title\"><a target=\"_blank\"href=\"blog.html?FileID="+files_fileid+"&FolderID="+files_folderid+"\">"+files_filename+text_draft+text_public+"</a><a target=\"_blank\"href=\"edit.html?FileID="+files_fileid+"&FolderID="+files_folderid+"\">"+"(Edit)"+"</a></div><div class=\"item-tags\"><div class=\"tag\">"+folders_name+"</div><div class=\"thumb-title\"><img src=\""+files_smallimg+"\"/></div></div></div></div>")
 			//$('.content-wrapper').prepend("<div class=\"grid-item teaching_plan\"><div class=\"description\"><div class=\"item-title\"><a href="">"+blogs_set[i].split('$$')[0]+"</div><div class=\"item-tags\"><div class=\"tag\">"+blogs_set[i].split('$$')[1]+"</div></div></div></div>")
@@ -35,5 +41,4 @@ $(function(){
 		}
 	});
 
-
-});
+}
